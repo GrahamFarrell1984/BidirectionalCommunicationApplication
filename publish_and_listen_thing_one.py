f4c79843c353a7ce35ab8960efcb2b9b57dc2608
Graham Farrell <grahamfarrell1984@gmail.com>
2020-03-08 13:56:51 +0000

    Tidy up scripts.

--- publish_and_listen_thing_one.py
+++ publish_and_listen_thing_one.py
@@ -20,73 +20,73 @@ led = 6 # Connect the LED to digital port D6
 grovepi.pinMode(led,"OUTPUT") # Set pin mode for port D6 as an output
 
 publisher_state = False # Set the publisher state to false. This is used in the while loop in the publish() method
-button_clicked = 0
 
+button_clicked = 0 # Set button_clicked as 0. This is used to initially set the LED as being turned off
+
+# Functions / Methods
+# Method to read from temperature sensor
 def read_temperature():
     [ temp_sensor_value,hum_sensor_value ] = dht(dht_sensor,dht_sensor_type) # Read the temperature and humidity sensor values
     temperature = str(temp_sensor_value) # Convert temperature sensor value to a String and store in a variable called temperature
-    return temperature # Return the value from the dht sensor
+    return temperature # Return the temperature value from the dht sensor
 
+# Method to read from sound sensor
 def read_sound():
     sound_sensor_value = grovepi.analogRead(sound_sensor) # Read the sound sensor value and store it in a variable called sound_sensor_value
     return sound_sensor_value # Return the value from the sound sensor
 
+# Method to read from potentiometer
 def read_potentiometer():
-    potentiometer_sensor_value = grovepi.analogRead(potentiometer_sensor) # Read the potentiometer sensor value and store it in a variable c$
+    potentiometer_sensor_value = grovepi.analogRead(potentiometer_sensor) # Read the potentiometer sensor value and store it in a variable called potentiometer_sensor_value
     return potentiometer_sensor_value # Return the value from the potentiometer
 
-# Method to listen for dweets from a specific thing called TestThingTwo
+# Method to listen for dweets from a specific thing called GrahamThingTwo
 def listen(publisher_thread): # The listen() method takes the publisher thread as a parameter
     print(listener_thread_name + " is Listening!") # Print Starting Listening!
     global publisher_state # Set publisher state as a global variable
     publisher_state = True # Set publisher state to true
     global button_clicked
-    print("Button Clicked 1: " + str(button_clicked))
     if not publisher_thread.is_alive(): # If publisher thread is not running execute the following code
         publisher_thread.start() # Start publisher thread
-    for dweet in dweepy.listen_for_dweets_from(thingTwoName): # For loop listens for dweets from a specific thing called TestThingTwo
-        print("Button Clicked 2: " + str(button_clicked))
+    for dweet in dweepy.listen_for_dweets_from(thingTwoName): # For loop listens for dweets from a specific thing called GrahamThingTwo
         content = dweet["content"] # Store the content from each dweet into a variable called content
-        print(str(content))
+        print(str(content)) # Print content
         try:
             button_clicked = content["ButtonClicked"]
-            print("Button Clicked 3: " + str(button_clicked))
         except:
-            print("An exception occurred")
+            print("Button not clicked yet!")
         thing = dweet["thing"] # Store the thing from each dweet into a variable called thing
-        print("Reading from TestThingTwo: " + str(content))
-        print(thing) # Print the variable called thing
-        print("")
+        print("Reading from " + str(thing) + ": " + str(content))
+        print("") # Adds an empty line in the terminal below our output above
 
         try:
-            print("Button Clicked String: " + str(button_clicked))
-            if int(button_clicked) == 1:
+            if int(button_clicked) == 1: # Check if the button has been pressed
                 print("True")
-                brightness = 255
+                brightness = 255 # Set maximum brightness
                 grovepi.analogWrite(led,brightness) # Give PWM output to LED
             else:
                 print("False")
-                brightness = 0
+                brightness = 0 # Set minimum brightness
                 grovepi.analogWrite(led,brightness) # Give PWM output to LED
         except:
-            print("An exception occurred")
+            print("Button still not clicked yet!")
     print("Listening Ending!") # Print Listening Ending!
 
-# Method to publish dweets from a specific thing called TestThingOne
+# Method to publish dweets from a specific thing called GrahamThingOne
 def publish(): # The publish() method takes no parameters
     print(publisher_thread_name + " is Publishing!") # Print Starting Publishing!
     while publisher_state: # While publisher state is true execute the following code
-        temperature = read_temperature()
-        sound = read_sound()
-        potentiometer = read_potentiometer()
+        temperature = read_temperature() # Call the read_temperature() function / method and store result in a variable called temperature
+        sound = read_sound() # Call the read_sound() function / method and store result in a variable called sound
+        potentiometer = read_potentiometer() # Call the read_potentiometer() function / method and store result in a variable called potentiometer
         result = dweepy.dweet_for(thingOneName, {"Temperature": temperature, "Sound": sound, "Potentiometer": potentiometer}) # Send a dweet$
-        print("TestThingOne published: " + str(result)) # Print the variable called result
+        print("GrahamThingOne published: " + str(result)) # Print the variable called result
         time.sleep(1) # Call the sleep() method from the time module and pass in 1 second as a parameter
-        print("")
+        print("") # Adds an empty line in the terminal below our output above
     print("Publishing Ending!") # Print Publishing Ending!
 
 publisher_thread = Thread(target=publish) # Create a new publisher thread passing in the publish() method as a parameter
-listener_thread = Thread(target=listen, args=(publisher_thread,)) # Create a new listener thread passing in the listen() method and publishe$
+listener_thread = Thread(target=listen, args=(publisher_thread,)) # Create a new listener thread passing in the listen() method and publisher_thread
 
 publisher_thread_name = publisher_thread.getName() # Get publisher thread name
 listener_thread_name = listener_thread.getName() # Get listener thread name
--- publish_and_listen_thing_two.py
+++ publish_and_listen_thing_two.py
@@ -24,52 +24,54 @@ full_angle = 300 # Full value of the rotary angle is 300 degrees, as per it's sp
 
 publisher_state = False # Set the publisher state to false. This is used in the while loop in the publish() method
 
+# Functions / Methods
+# Method to read from humidity sensor
 def read_humidity():
     [ temp_sensor_value,hum_sensor_value ] = dht(dht_sensor,dht_sensor_type) # Read the temperature and humidity sensor values
     humidity = str(hum_sensor_value) # Convert humidity sensor value to a String and store in a variable called humidity
-    return humidity
+    return humidity # Return the humidity value from the dht sensor
 
+# Method to read from light sensor
 def read_light():
     light_sensor_value = grovepi.analogRead(light_sensor) # Read the light sensor value and store it in a variable called light_sensor_value
     return light_sensor_value # Return the value from the light sensor
 
-# Method to listen for dweets from a specific thing called TestThingOne
+# Method to listen for dweets from a specific thing called GrahamThingOne
 def listen(publisher_thread): # The listen() method takes the publisher thread as a parameter
     print(listener_thread_name + " is Listening!") # Print Starting Listening!
     global publisher_state # Set publisher state as a global variable
     publisher_state = True # Set publisher state to true
     if not publisher_thread.is_alive(): # If publisher thread is not running execute the following code
         publisher_thread.start() # Start publisher thread
-    for dweet in dweepy.listen_for_dweets_from(thingOneName): # For loop listens for dweets from a specific thing called TestThingOne
+    for dweet in dweepy.listen_for_dweets_from(thingOneName): # For loop listens for dweets from a specific thing called GrahamThingOne
         content = dweet["content"] # Store the content from each dweet into a variable called content
-        sensor_value = content["Potentiometer"]
-        print("Potentiometer Reading: " + str(sensor_value))
+        sensor_value = content["Potentiometer"] # Get the value from the potentiometer
+        print("Potentiometer Reading: " + str(sensor_value)) # Print the value from the potentiometer
         thing = dweet["thing"] # Store the thing from each dweet into a variable called thing
-        print("Reading from TestThingOne: " + str(content))
-        print(thing) # Print the variable called thing
-        print("")
+        print("Reading from " + str(thing) + ": " + str(content))
+        print("") # Adds an empty line in the terminal below our output above
         voltage = round((float)(sensor_value) * adc_ref / 1023, 2) # Calculate voltage
         degrees = round((voltage * full_angle) / grove_vcc, 2) # Calculate rotation in degrees (0 to 300)
         brightness = int(degrees / full_angle * 255) # Calculate LED brightess (0 to 255) from degrees (0 to 300)
-        print("Brightness: " + str(brightness))
+        print("Brightness: " + str(brightness)) # Print brightness value
         grovepi.analogWrite(led,brightness) # Give PWM output to LED
 
     print("Listening Ending!") # Print Listening Ending!
 
-# Method to publish dweets from a specific thing called TestThingTwo
+# Method to publish dweets from a specific thing called GrahamThingTwo
 def publish(): # The publish() method takes no parameters
     print(publisher_thread_name + " is Publishing!") # Print Starting Publishing!
     while publisher_state: # While publisher state is true execute the following code
-        humidity = read_humidity()
-        light = read_light()
-        result = dweepy.dweet_for(thingTwoName, {"Humidity": humidity, "Light": light}) # Send a dweet from a specific thing called TestThin$
-        print("TestThingTwo published: " + str(result)) # Print the variable called result
+        humidity = read_humidity() # Call the read_humidity() function / method and store result in a variable called humidity
+        light = read_light() # Call the read_light() function / method and store result in a variable called light_sensor
+        result = dweepy.dweet_for(thingTwoName, {"Humidity": humidity, "Light": light}) # Send a dweet from a specific thing called GrahamThingTwo
+        print("GrahamThingTwo published: " + str(result)) # Print the variable called result
         time.sleep(1) # Call the sleep() method from the time module and pass in 1 second as a parameter
-        print("")
+        print("") # Adds an empty line in the terminal below our output above
     print("Publishing Ending!") # Print Publishing Ending!
 
 publisher_thread = Thread(target=publish) # Create a new publisher thread passing in the publish() method as a parameter
-listener_thread = Thread(target=listen, args=(publisher_thread,)) # Create a new listener thread passing in the listen() method and publishe$
+listener_thread = Thread(target=listen, args=(publisher_thread,)) # Create a new listener thread passing in the listen() method and publisher_thread
 
 publisher_thread_name = publisher_thread.getName() # Get publisher thread name
 listener_thread_name = listener_thread.getName() # Get listener thread name
